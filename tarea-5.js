d95759d88107febc708b563612a9a76742a01924
matiboettner <matirc2004@gmail.com>
2022-05-04 11:58:31 -0300

    crea js y html
    este commit crea el js y el html de la tarea

---
+++ tarea-5.js
@@ -0,0 +1,12 @@
+function calcularSalarioMensual(salarioAnual){
+    const MESES_EN_EL_ANIO = 12;
+    return salarioAnual / MESES_EN_EL_ANIO;
+}
+
+const $calcularSalarioMensual = document.querySelector('#calcular-salario-mensual');
+
+$calcularSalarioMensual.onclick = function(){
+    const salarioAnual = Number(document.querySelector('#salario-anual').value);
+    const salarioMensual = calcularSalarioMensual(salarioAnual);
+    document.querySelector('#salario-mensual').value = salarioMensual;
+}
